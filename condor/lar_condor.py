f11edb667e0b7b68e83f910d24aed8106a10e445
Alessandro Thea <alessandro.thea@cern.ch>
2025-06-20 21:44:01 +0200

    Committing condor work

--- condor/lar_condor.py
+++ condor/lar_condor.py
@@ -7,7 +7,7 @@ import yaml
 from pathlib import Path
 import os.path
 
-from pydantic import BaseModel, FilePath, DirectoryPath, field_validator
+from pydantic import BaseModel, FilePath, DirectoryPath, field_validator, model_validator
 from typing import Optional, List
 import htcondor
 
@@ -15,13 +15,14 @@ import htcondor
 
 class JobConfig(BaseModel):
     """
-    Modle for 
+    Model of Larsoft jobs 
     """
+    label: str
     larsoft_runner: FilePath
-    # work_area: DirectoryPath
     config_fcl: FilePath
 
-    n_events: int
+    n_events: Optional[int] = -1
+    n_jobs_per_file: Optional[int] = 1
     output_file_prefix: Optional[str]
     eos_output_folder : DirectoryPath
     eos_input_files: Optional[List[FilePath]] = None
@@ -48,6 +49,15 @@ class JobConfig(BaseModel):
             path_list.append(value)
         return path_list
 
+
+    @model_validator(mode='after')
+    def check_events_and_jobs(self):
+        if self.n_jobs_per_file != 1 and self.n_events == -1:
+            raise ValueError('Multiple jobs per file require the number of events per file to be defined')
+        return self
+
+
+
 #------------------------------------------------------------------------------
 
 def to_eos(p):
@@ -62,10 +72,10 @@ def cli(card_file, submit):
 
     cfg = JobConfig(**data_loaded)
 
-    # args = [cfg.work_area, '-c', cfg.config_fcl, ]
-    args = ['-c', cfg.config_fcl, ]
-    if not cfg.n_events is None:
-        args += ['-n', cfg.n_events]
+    args = ['-c', cfg.config_fcl]
+
+    # if not cfg.n_events is None:
+    # args += ['-n', cfg.n_events]
 
     job_files = {}
     if cfg.eos_input_files is None:
@@ -88,51 +98,61 @@ def cli(card_file, submit):
         'log'       : 'larsoft.$(ClusterId).log',
         'transfer_executable' : 'false',
         'should_transfer_files': 'YES',
-        'output_destination':  to_eos(cfg.eos_output_folder) + '/$(ClusterId)/job_$(ProcId)/',
-        # '+JobFlavour': '"longlunch"'.
+        'output_destination':  to_eos(cfg.eos_output_folder) + f'/{cfg.label}_$(ClusterId)/job_$(job_index)/',
+        '+JobFlavour': '"tomorrow"',
         'MY.SendCredential': 'True',
         'MY.XRDCP_CREATE_DIR':  'True',
         'MY.SingularityImage':  '"/cvmfs/unpacked.cern.ch/registry.hub.docker.com/fermilab/fnal-dev-sl7:latest"',
     })
 
+    # Add (parameterised) input files transfer if needed
     if not cfg.eos_input_files is None:
         sub['transfer_input_files'] = '$(input_file)'
 
-    print(sub)
 
+    # Create job parameters
     itemdata = []
+
+    n_events_per_job = cfg.n_events // cfg.n_jobs_per_file
+
+    digits_file = len(str(len(job_files)-1))
+    digits_job = len(str((cfg.n_jobs_per_file*len(job_files))-1))
+
+
     for i,f in job_files:
+        for k in range(cfg.n_jobs_per_file):
 
-        print(f"[red]Creating job {i}[/red]")
-        job_args = args + (['-s', f.name] if not f is None else [])
+            print(f"[cyan]Creating job: file {i} subjob {k}[/cyan]")
 
-        if not cfg.output_file_prefix is None:
-            job_args += ['-o', f'{cfg.output_file_prefix}_{i}.root']   
+            # file_index =  f'{i:02d}'
+            # job_index =  f'{k:02d}'
+            file_index = '{num:0{width}}'.format(num=i, width=digits_file)
+            job_index = '{num:0{width}}'.format(num=i*cfg.n_jobs_per_file+k, width=digits_job)
 
-        
-        item_extra = {
-                'job_args': ' '.join([ str(a) for a in job_args])
-            }
+            item_extra = {'job_index' : job_index}
 
-        if not f is None:
-            item_extra['input_file'] = to_eos(f)
+            job_args = args + (['-s', f.name] if not f is None else [])
 
-        itemdata.append(item_extra)
+            job_args += ['-n', n_events_per_job]
 
-        # sub = htcondor.Submit({
-        #     'executable': str(cfg.larsoft_runner),
-        #     'arguments' : ' '.join([ str(a) for a in job_args]),
-        #     'error'     : 'larsoft.$(ClusterId).$(ProcId).err',
-        #     'output'    : 'larsoft.$(ClusterId).$(ProcId).out',
-        #     'log'       : 'larsoft.$(ClusterId).log',
-        #     'should_transfer_files': 'YES',
-        #     'output_destination':  to_eos(cfg.eos_output_folder) + '/$(ClusterId)/',
-        #     # '+JobFlavour': '"longlunch"'.
-        #     'MY.SendCredential': 'True',
-        #     'MY.XRDCP_CREATE_DIR':  'True',
-        #     'MY.SingularityImage':  '"/cvmfs/unpacked.cern.ch/registry.hub.docker.com/fermilab/fnal-dev-sl7:latest"',
-        
-        # })
+            if cfg.n_jobs_per_file != 1:
+                job_args += ['--nskip', k*n_events_per_job]
+
+            if not cfg.output_file_prefix is None:
+                job_args += ['-o', f'{cfg.output_file_prefix}_{job_index}.root']   
+            
+            item_extra.update({
+                    'job_args': ' '.join([ str(a) for a in job_args])
+                })
+
+            if not f is None:
+                item_extra['input_file'] = to_eos(f)
+
+            itemdata.append(item_extra)
+
+
+    # Print cluster definition
+    print(sub)
     print(itemdata)
 
     if submit:
